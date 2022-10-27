b54b96c9b5f47a3592c20a629ee04ceccf2a2de5
Simen Dymbe <simen.dymbe@gmail.com>
2022-10-27 18:30:01 +0200

    Changed main

--- workdayCalendar/main.cpp
+++ workdayCalendar/main.cpp
@@ -6,29 +6,12 @@
 
 int main() {
 	WorkdayCalendar wc = WorkdayCalendar();
+	DateTime date1 = DateTime(2022, 10, 29, 8, 0, 0);
+	
+	DateTime date2 = wc.getEndDateTime(date1, 4.5);
+	std::cout << date2;
 
-
-	DateTime date1 = DateTime(2022, 10, 29, 15, 23, 34);
-	DateTime date2 = DateTime(2022, 2, 28, 15, 23, 34);
-
-	//wc.getEndDateTime(date1, (float)5.32);
-	std::cout << "Start" << std::endl;
-	std::cout << date1 << std::endl;
-	date1.subtractWorkHours(8);
-	std::cout << date1 << std::endl;
-	date1.subtractWorkDays(5);
-	std::cout << date1 << std::endl;
-	//date1.subtractWorkHours(465);
-	//std::cout << date1 << std::endl;
-
-
-	std::cout << "Date before" << std::endl;
-	std::cout << date1.daysSince(date2) << std::endl;
-
-	std::cout << "Days since sunday" << std::endl;
-	std::cout << date2.daysSinceSunday() << std::endl;
-
-	int p = 3;
+	int p;
 	std::cin >> p;
 
 	return 0;
