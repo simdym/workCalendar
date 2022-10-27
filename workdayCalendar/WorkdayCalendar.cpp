db5b3f5c0736c374b0bdb8b47b218050cca247d0
Simen Dymbe <simen.dymbe@gmail.com>
2022-10-27 18:27:40 +0200

    Changed main

--- workdayCalendar/WorkdayCalendar.cpp
+++ workdayCalendar/WorkdayCalendar.cpp
@@ -5,27 +5,16 @@ using namespace std::chrono;
 DateTime WorkdayCalendar::getEndDateTime(DateTime startDate, float fullDaysWorked)
 {	
 	int workingDays = (int)fullDaysWorked;
-
 	int workingHours = (int)(8 * (fullDaysWorked - (int)fullDaysWorked));
 	int workingMin = (int)(60 * (fullDaysWorked * 8 - (int)(fullDaysWorked * 8)));
 	int workingSec = (int)(60 * (fullDaysWorked * 8 * 60 - (int)(fullDaysWorked * 8 * 60)));
 
-	std::cout << workingDays << ":" << workingHours << ":" << workingMin << ":" << workingSec;
+	startDate.addWorkDays(workingDays);
+	startDate.addWorkHours(workingHours);
+	startDate.addWorkMinutes(workingMin);
+	startDate.addWorkSeconds(workingSec);
 
-
-	int nonFullWeeks = workingDays % 7;
-	int hoursSince8 = 1;
-	int sinceSunday = startDate.daysSinceSunday();
-	if (sinceSunday <= 5) {
-		startDate.subtractDays(nonFullWeeks);
-	}
-	else {
-		startDate.subtractDays(nonFullWeeks + 2); //Plus the weekend
-	}
-
-
-
-	return DateTime(0, 0, 0, 0, 0, 0);
+	return startDate;
 }
 
 time_point<system_clock> WorkdayCalendar::toChrono(int y, int m, int d, int hh, int mm)
